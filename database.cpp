a19416a733ddb5c337826ab4c489ae2d8c67af3f
LucasLefevre <lucas.lefevre@hotmail.com>
2015-12-16 17:33:06 +0100

    added comments

--- database.cpp
+++ database.cpp
@@ -8,17 +8,14 @@ using namespace std;
 
 Database::Database(string filename) {
 	
-	cout << "Loading database\n";
 	this->filename = filename;
 	loadHeader(filename);
 	printInfos();
 	loadProteins(filename);
 	loadHeaders(filename);
-	
 }
 
 void Database::loadHeader(const string &filename) {
-	
 	
 	ifstream dbFile;
 	dbFile.open(filename + ".pin", ios::binary | ios::in);
@@ -87,72 +84,51 @@ void Database::loadHeader(const string &filename) {
 			dbFile.read((char*) &offsetB, sizeof(offsetB));
 			sequenceOffsets.push_back(bytesToIntBigEndian(offsetB, 4)); //convert bytes and store them as integer
 		}
-		
-		dbFile.close();	
-
-		
-		
-		
+		dbFile.close();		
 	}
 	else {
 		cout << "Unable to open database file : " << filename << ".pin\n";
 		throw string("Unable to load file");
 	}
-		
-	
 }
 
 void Database::loadProteins(const string &filename) {
-	
+	//load sequences from file
 	
 	if (!headerOffsets.empty() && !sequenceOffsets.empty()) {
-		
 		
 		ifstream dbFile;
 		dbFile.open(filename + ".psq", ios::binary | ios::in);
 		dbFile.seekg(0, ios::beg);
 		
 		if (dbFile.is_open()) {
-			
-			cout << "Loading proteins\n";
 	
-			
-			
 			int sequenceSize;
 			for (int i = 0; i <= nbrSequences-1 ; i++){  //nbrSequences + 1
 				
-				char firstByte[1];
-				dbFile.read((char*) &firstByte, sizeof(firstByte));
+				char firstByte[1];  
+				dbFile.read((char*) &firstByte, sizeof(firstByte));  // go one byte forward to avoid the null byte at the beginning
 				
 				sequenceSize = sequenceOffsets[i+1] - sequenceOffsets[i] -1  ;
 				char* sequence = new char[sequenceSize];
 				dbFile.read(sequence, sequenceSize);
 				Protein * prot = new Protein();
-				//vector<char> sequenceVector(sequence, sequence + sequenceSize);
-				
-				//sequenceVector.pop_back(); //remove the null byte between sequences
-				
 				prot->setSequence(sequence, sequenceSize);
-				
 				proteins.push_back(*prot);
 			}
-			
-
 		}
-		else cout << "Unable to open database file : " << filename << ".psq\n";
-		
-		
-	
+		else {
+			cout << "Unable to open database file : " << filename << ".psq\n";
+			throw string("Unable to load file");
+		}
 	}
 	else cout << "Header offsets or sequence offsets not yet loaded\n";
-
 }
 
 void Database::loadHeaders(const string &filename) {
-	
+	//load headers from file
 	
 	if (!headerOffsets.empty() && !sequenceOffsets.empty()) {
-		
 		
 		ifstream dbFile;
 		dbFile.open(filename + ".phr", ios::binary | ios::in);
@@ -170,17 +146,14 @@ void Database::loadHeaders(const string &filename) {
 				dbFile.read((char*) &header, headerSize);
 				
 				proteins[i].setHeader(string(header).substr(7, string::npos));
-				
-				
 			}
 		}
-		else cout << "Unable to open database file : " << filename << ".phr\n";
-		
-		
-	
+		else {
+			cout << "Unable to open database file : " << filename << ".phr\n";
+			throw string("Unable to load file");
+		}
 	}
 	else cout << "Header offsets or sequence offsets not yet loaded\n";
-
 }
 
 int Database::bytesToIntLittleEndian(char bytes[], int lenght) {
@@ -228,20 +201,16 @@ Protein & Database::getProtein(int index) {
 	}
 }
 
-
-
-
 void Database::printInfos(std::ostream & out) {
-	out << " - Version : " <<  version << "\n";
+	//out << " - Version : " <<  version << "\n";
 	out << " - Type : " << type  << "\n";
-	out << " - Title lenght : " << titleLength  << "\n";
+	//out << " - Title lenght : " << titleLength  << "\n";
 	out << " - Title : " << title << "\n";
-	out << " - Timestamp Length : " <<  timestampLength  << "\n";
+	//out << " - Timestamp Length : " <<  timestampLength  << "\n";
 	out << " - Timestamp : " << timestamp  << "\n";
 	out << " - Number of sequences : "  <<  nbrSequences << "\n";
 	out << " - Residue count : " <<  residueCount << "\n";
 	out << " - Longuest sequence : " << maxLength << "\n";
-		
 }
 
 int Database::getNbrSequences() {
